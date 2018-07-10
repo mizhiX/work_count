bda2dedc868d93c23903fed1480a14f5c199f8e4
Unknown <1925777036@qq.com>
2018-07-10 11:39:05 +0800

    添加了源代码文件

---
+++ zhengyiming/functions.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(ruquest):
+    return render(ruquest, 'home.html')
+
+
+
+def count(ruquest):
+    text = ruquest.GET['text']
+    total_count = len(ruquest.GET['text'])
+    texts_dict = {}
+    for texts in text:
+        if texts not in texts_dict:
+            texts_dict[texts] = 1
+        else:
+            texts_dict[texts] += 1
+    sorted_dict = sorted(texts_dict.items(), key=lambda w:w[1], reverse=True)
+
+
+    return render(ruquest, 'count.html', {'count':total_count, 'sortedict':sorted_dict})
