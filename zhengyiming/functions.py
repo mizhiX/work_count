2a1ec0368e1b85bce815a2c1d3a8435745f797ea
Unknown <1925777036@qq.com>
2018-07-10 16:58:33 +0800

    检查与改正错误

--- zhengyiming/functions.py
+++ zhengyiming/functions.py
@@ -2,14 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def home(ruquest):
-    return render(ruquest, 'home.html')
+def home(request):
+    return render(request, 'home.html')
 
 
 
-def count(ruquest):
-    text = ruquest.GET['text']
-    total_count = len(ruquest.GET['text'])
+def count(request):
+    text = request.GET['text']
+    total_count = len(request.GET['text'])
     texts_dict = {}
     for texts in text:
         if texts not in texts_dict:
@@ -19,7 +19,7 @@ def count(ruquest):
     sorted_dict = sorted(texts_dict.items(), key=lambda w:w[1], reverse=True)
 
 
-    return render(ruquest, 'count.html', {'count':total_count, 'sortedict':sorted_dict})
+    return render(request, 'count.html', {'count':total_count, 'sortedict':sorted_dict})
 
-def about(ruquest):
-    return render(ruquest, 'about.html')
+def about(request):
+    return render(request, 'about.html')
